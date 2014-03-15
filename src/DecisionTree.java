57325283086fd55fdd14bcaaed2cce15a91c6fc1
wangdaiwei <wangdaiwei21@gmail.com>
2014-03-15 22:03:56 +0800

    Update some files.

---
+++ src/DecisionTree.java
@@ -0,0 +1,28 @@
+// The main class of the decision tree.
+// Create on 15/3/14.
+
+import java.util.*;
+
+public class DecisionTree {
+	// The input dataset.
+	// Each ArrayList in the hash set stands for an instance.
+	// Replace the ArrayList with other class in the future.
+	private HashSet< ArrayList<String> > dataset;
+
+	// Calculate the GINI value of the input attribute.
+	// The input should be two ArrayList.
+	// attribute_array stores the training instances' values of the selected attribute.
+	// label_array stores all the instances' labels.
+	// Returns the GINI value of the selected attribute.
+	public double GINI ( ArrayList<String> attribute_array, ArrayList<String> label_array ) {
+		return 0.0;
+	}
+
+	// Read the training data file into the hash set.
+	public void readTrainingFile () {
+	}
+
+	public void createDecisionTree () {
+		
+	}
+}
